2a16d61b03073a3bcad43d98f923022753e141f8
Silvia Rocha <silviadprocha@gmail.com>
2020-06-20 17:30:17 +0200

    Updates

---
+++ precache-manifest.8584ada5d6b56735f74b5ed2717e6073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928aab980b863470da9664053715c3ae",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "79c7fc9a32a0a3bac397",
+    "url": "/todolist-react/static/css/main.c207d72b.chunk.css"
+  },
+  {
+    "revision": "c2823383ad26f7ac889e",
+    "url": "/todolist-react/static/js/2.b2ee15e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.b2ee15e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c7fc9a32a0a3bac397",
+    "url": "/todolist-react/static/js/main.5d69a357.chunk.js"
+  },
+  {
+    "revision": "0eed23ab9c1895acb675",
+    "url": "/todolist-react/static/js/runtime-main.56464843.js"
+  }
+]);
